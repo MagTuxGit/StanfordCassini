1a9a719030689217bcd7739cacdc7a912e172487
TrAnd <magtuxgit@gmail.com>
2017-04-06 11:43:34 +0300

    show some image

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -9,6 +9,46 @@
 import UIKit
 
 class ImageViewController: UIViewController {
-    var imageURL: URL?
-
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                // don't fetch any if MVC is not on the screen
+                fetchImage()
+            }
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            if let urlContents = try? Data(contentsOf: url) {
+                image = UIImage(data: urlContents)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+        imageURL = DemoURL.stanford
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
+    }
+    
+    private var imageView = UIImageView()   //frame: CGRect.zero by default
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
 }
