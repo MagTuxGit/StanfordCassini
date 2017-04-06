acd4a642cbc423d2fd1b6f961c89dc4f902ece8f
TrAnd <magtuxgit@gmail.com>
2017-04-06 11:04:09 +0300

    files arranged

---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Andrij Trubchanin on 4/6/17.
+//  Copyright © 2017 Andrij Trubchanin. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    var imageURL: URL?
+
+}
